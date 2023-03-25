e4b6418c8c293eb6c1c4858fca667d87e0dc383d
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-03-25 19:47:02 -0300

    Update main.tf

--- s3-bucket-module/main.tf
+++ s3-bucket-module/main.tf
@@ -10,6 +10,17 @@ resource "aws_s3_bucket" "this" {
 
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = var.enable_sse ? var.sse_algorithm : "AES256"
+      kms_master_key_id = var.enable_sse ? var.kms_master_key_id : null
+    }
+  }
+}
+
 ##########################
 # Bucket private access
 ##########################
