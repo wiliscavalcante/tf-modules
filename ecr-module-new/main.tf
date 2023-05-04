3215b18cbb192e8710562b67c56726e9876ee15c
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-03 23:32:15 -0300

    Update main.tf

--- ecr-module-new/main.tf
+++ ecr-module-new/main.tf
@@ -1,7 +1,7 @@
 resource "aws_ecr_repository" "ecr_repo" {
   name                 = var.repository_name
   image_scanning_configuration {
-    scan_on_push       = var.scan_on_push
+    scan_on_push = var.scan_on_push
   }
   image_tag_mutability = var.image_tag_mutability
 
