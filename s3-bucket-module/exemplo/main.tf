2617cfd00abff9faf25e130ebf03c991b7c56fd7
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-03 22:24:00 -0300

    Update main.tf

--- s3-bucket-module/exemplo/main.tf
+++ s3-bucket-module/exemplo/main.tf
@@ -11,11 +11,19 @@ locals {
 # Create the S3 Bucket
 # ---------------------------------------------------------------------------------------------------------------------
 
-module "s3_bucket" {
-  source = "../"
+module "s3_bucket_01" {
+  source = "github.com/wiliscavalcante/tf-modules.git//s3-bucket-module"
 
-  count  = length(var.s3_bucket_names) 
-  bucket = var.s3_bucket_names[count.index]
+  bucket_name = "bucket-terraform-01"
+  force_destroy = true
+  tags          = var.tags
+
+}
+
+module "s3_bucket_02" {
+  source = "github.com/wiliscavalcante/tf-modules.git//s3-bucket-module"
+
+  bucket_name = "bucket-terraform-02"
   force_destroy = true
   tags          = var.tags
 
