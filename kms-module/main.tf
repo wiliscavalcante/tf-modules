57128be720140ee77aaa7f555e7f5d607bdf957a
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-04 11:07:05 -0300

    Update main.tf

--- kms-module/main.tf
+++ kms-module/main.tf
@@ -1,9 +1,14 @@
 resource "aws_kms_key" "this" {
-  description     = var.description
-  policy          = var.policy
+  description            = var.description
   deletion_window_in_days = var.deletion_window_in_days
-  key_rotation_period = var.key_rotation_period
-  customer_master_key_spec = var.customer_master_key_spec
+  key_usage              = "ENCRYPT_DECRYPT"
+  customer_master_key_spec = "SYMMETRIC_DEFAULT"
+  enable_key_rotation    = true
+  tags                   = var.tags
+  policy                 = var.policy
 
-  tags = var.tags
+  alias {
+    name = var.alias_name
+  }
 }
+
