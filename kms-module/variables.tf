6399c52a58c6a629973c70e2d9b797c5a346484c
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-04 11:16:36 -0300

    Update variables.tf

--- kms-module/variables.tf
+++ kms-module/variables.tf
@@ -1,50 +1,49 @@
-variable "description" {
-  type    = string
-  default = "Example KMS Key"
-}
-
-variable "policy" {
-  type    = string
-  default = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {"AWS": "*"},
-      "Action": [
-        "kms:Encrypt",
-        "kms:Decrypt",
-        "kms:ReEncrypt*",
-        "kms:GenerateDataKey*",
-        "kms:DescribeKey"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
 variable "deletion_window_in_days" {
-  type    = number
-  default = 30
-}
-
-variable "key_rotation_period" {
-  type    = string
-  default = "1y"
-}
-
-variable "customer_master_key_spec" {
-  type    = string
-  default = "SYMMETRIC_DEFAULT"
+  type        = number
+  default     = 30
+  description = "O período de carência em dias antes da remoção definitiva da chave KMS."
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {
-    Terraform   = "true"
-    Environment = "dev"
+  type        = map(string)
+  default     = {
+    Environment = "Production"
   }
+  description = "As tags a serem adicionadas na chave KMS."
+}
+
+variable "policy" {
+  type        = string
+  default     = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "${data.aws_caller_identity.current.arn}"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+  description = "A política de acesso à chave KMS."
+}
+
+variable "alias_name" {
+  type        = string
+  default     = ""
+  description = "O nome do alias a ser criado para a chave KMS."
+}
+
+variable "description" {
+  type        = string
+  default     = "Example KMS Key"
+  description = "A descrição da chave KMS."
 }
