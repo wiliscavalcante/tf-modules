d4b8cff951a10ace6f3648ebf073f216b27c5c4b
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-04 13:56:34 -0300

    Update main.tf

--- kms-module/main.tf
+++ kms-module/main.tf
@@ -1,15 +1,16 @@
 resource "aws_kms_key" "this" {
-  description            = var.description
+  customer_master_key_spec = var.customer_master_key_spec
   deletion_window_in_days = var.deletion_window_in_days
-  key_usage              = var.key_usage
-  customer_master_key_spec           = var.customer_master_key_spec
+  description            = var.description
   enable_key_rotation    = var.enable_key_rotation
-  multi_region                       = var.multi_region
+  key_usage              = var.key_usage
+  multi_region           = var.multi_region
+  policy                 = jsonencode(var.policy)
   tags                   = var.tags
-  policy = jsonencode(var.policy)
 }
-# Cria um alias para a chave KMS, se houver um nome especificado
+
 resource "aws_kms_alias" "this" {
   name          = "alias/${var.alias_name}"
   target_key_id = aws_kms_key.this.key_id
 }
+
