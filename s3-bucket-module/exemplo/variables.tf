56655b3d3116d847b426717c69ac09eb242d705e
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-03 22:24:23 -0300

    Update variables.tf

--- s3-bucket-module/exemplo/variables.tf
+++ s3-bucket-module/exemplo/variables.tf
@@ -6,10 +6,3 @@ variable "tags" {
     terraform   = "true"
   }
 }
-
-
-variable "s3_bucket_names" {
-  description = "Nomes dos buckets"  
-  type    = list(any)
-  default = ["cavalcantech-tf01", "cavalcantech-tf02", "cavalcantech-tf03"]
-}
