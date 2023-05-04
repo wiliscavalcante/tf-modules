da741d75bf25891feea7b69a20363d238ede5e9a
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-03 23:38:49 -0300

    Update main.tf

--- ecr-module-new/main.tf
+++ ecr-module-new/main.tf
@@ -1,14 +1,21 @@
 resource "aws_ecr_repository" "ecr_repo" {
-  name                 = var.repository_name
+  name = var.repository_name
+
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
   }
+
   image_tag_mutability = var.image_tag_mutability
 
-  dynamic "lifecycle_policy" {
-    for_each = var.lifecycle_policy_enabled == true ? [1] : []
-    content {
-      policy_text = var.lifecycle_policy_text
+  # Adicionado bloco de política de ciclo de vida
+  lifecycle_policy {
+    ignore_existing_tags = true
+    # Permite que a política de ciclo de vida seja opcional
+    dynamic "policy" {
+      for_each = var.lifecycle_policy_enabled ? [var.lifecycle_policy_text] : []
+      content {
+        policy_text = policy.value
+      }
     }
   }
 }
