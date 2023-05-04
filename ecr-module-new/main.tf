57473d4573277dd84e8f8cfcdee1fa94af17fe84
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-04 09:09:21 -0300

    Update main.tf

--- ecr-module-new/main.tf
+++ ecr-module-new/main.tf
@@ -9,6 +9,7 @@ resource "aws_ecr_repository" "this" {
   tags = var.tags
 }
 resource "aws_ecr_lifecycle_policy" "this" {
+  count = var.enable_lifecycle_policy ? 1 : 0
   repository = aws_ecr_repository.this.name
 
   policy = <<EOF
