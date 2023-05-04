7d421087863b5c5c20aac0f4fde91b1607b27335
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-04 13:50:29 -0300

    Update variables.tf

--- kms-module/variables.tf
+++ kms-module/variables.tf
@@ -28,3 +28,18 @@ variable "description" {
   default     = "Example KMS Key"
   description = "A descrição da chave KMS."
 }
+variable "key_usage" {
+  description = "Especifica o uso pretendido da chave. Valores válidos: `ENCRYPT_DECRYPT` ou `SIGN_VERIFY`. O valor padrão é `ENCRYPT_DECRYPT`."
+  type        = string
+  default     = "ENCRYPT_DECRYPT"
+}
+variable "customer_master_key_spec" {
+  description = "Especifica se a chave contém uma chave simétrica ou um par de chaves assimétricas e os algoritmos de criptografia ou assinatura que a chave suporta. Valores válidos: `SYMMETRIC_DEFAULT`, `RSA_2048`, `RSA_3072`, `RSA_4096`, `HMAC_256`, `ECC_NIST_P256`, `ECC_NIST_P384`, `ECC_NIST_P521`, ou `ECC_SECG_P256K1`. O padrão é `SYMMETRIC_DEFAULT`"
+  type        = string
+  default     = "SYMMETRIC_DEFAULT"
+}
+variable "enable_key_rotation" {
+  description = "Sespecifica se a rotação de chave está habilitada. O valor padrão é true."
+  type        = bool
+  default     = true
+}
