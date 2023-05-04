8c9df9de32bf04befbf6eb115b9fc6a28a1cbbb3
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-04 15:32:48 -0300

    Update outputs.tf

--- kms-module/outputs.tf
+++ kms-module/outputs.tf
@@ -1,41 +1,23 @@
-# outputs.tf
-
-output "kms_key_id" {
-  value = module.kms_key.key_id
+output "key_id" {
+  value = aws_kms_key.this.key_id
 }
 
-output "kms_key_arn" {
-  value = module.kms_key.arn
+output "key_arn" {
+  value = aws_kms_key.this.arn
 }
 
-output "kms_key_alias" {
-  value = module.kms_key.alias
+output "key_alias" {
+  value = aws_kms_alias.this.name
 }
 
-output "kms_key_arn_alias" {
-  value = module.kms_key.arn_alias
+output "key_rotation_enabled" {
+  value = aws_kms_key.this.enable_key_rotation
 }
 
-output "kms_key_description" {
-  value = module.kms_key.description
+output "key_multi_region" {
+  value = aws_kms_key.this.multi_region
 }
 
-output "kms_key_usage" {
-  value = module.kms_key.key_usage
-}
-
-output "kms_customer_master_key_spec" {
-  value = module.kms_key.customer_master_key_spec
-}
-
-output "kms_enable_key_rotation" {
-  value = module.kms_key.enable_key_rotation
-}
-
-output "kms_multi_region" {
-  value = module.kms_key.multi_region
-}
-
-output "kms_policy" {
-  value = module.kms_key.policy
+output "key_policy" {
+  value = aws_kms_key.this.policy
 }
