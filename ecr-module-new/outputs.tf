d298a5b3847fdbaea87bf96e0ef16512ff43ea04
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-03 23:48:29 -0300

    Update outputs.tf

--- ecr-module-new/outputs.tf
+++ ecr-module-new/outputs.tf
@@ -1,4 +1,4 @@
-output "ecr_repository_url" {
-  description = "A URL do repositório do ECR."
-  value       = module.ecr_repository.ecr_repository_url
-}
+# output "ecr_repository_url" {
+#   description = "A URL do repositório do ECR."
+#   value       = module.ecr_repository.ecr_repository_url
+# }
