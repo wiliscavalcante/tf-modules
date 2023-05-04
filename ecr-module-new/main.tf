e9faa5f2bc378d81b885d1c33ace80951eb72e8f
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-04 09:52:30 -0300

    Update main.tf

--- ecr-module-new/main.tf
+++ ecr-module-new/main.tf
@@ -8,30 +8,7 @@ resource "aws_ecr_repository" "this" {
 
   tags = var.tags
 }
-# resource "aws_ecr_lifecycle_policy" "this" {
-#   count = var.enable_lifecycle_policy ? 1 : 0
-#   repository = aws_ecr_repository.this.name
 
-#   policy = <<EOF
-# {
-#     "rules": [
-#         {
-#             "rulePriority": 1,
-#             "description": "Keep last 30 images",
-#             "selection": {
-#                 "tagStatus": "tagged",
-#                 "tagPrefixList": ["v"],
-#                 "countType": "imageCountMoreThan",
-#                 "countNumber": 30
-#             },
-#             "action": {
-#                 "type": "expire"
-#             }
-#         }
-#     ]
-# }
-# EOF
-# }
 resource "aws_ecr_lifecycle_policy" "this" {
   count = var.enable_lifecycle_policy ? 1 : 0
 
@@ -43,7 +20,7 @@ resource "aws_ecr_lifecycle_policy" "this" {
         rulePriority = 1
         description  = "Keep images for ${var.days_to_keep} days"
         selection    = {
-          tagStatus    = "tagged"
+          tagStatus    = "any"
           countType    = "sinceImagePushed"
           countNumber  = var.days_to_keep
           countUnit    = "days"
