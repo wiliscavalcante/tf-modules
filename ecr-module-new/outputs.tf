26f49619b62a96ac5b2247aac5f190c055d87e18
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-04 00:47:29 -0300

    Update outputs.tf

--- ecr-module-new/outputs.tf
+++ ecr-module-new/outputs.tf
@@ -7,3 +7,7 @@ output "registry_id" {
   value       = aws_ecr_repository.this.registry_id
   description = "ID do registro no ECR"
 }
+output "lifecycle_policy_id" {
+  value       = aws_ecr_lifecycle_policy.this.id
+  description = "ID do lifecycle policy associado ao repositório ECR"
+}
