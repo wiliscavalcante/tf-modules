e05e5e41985d1da476b354d3aed7f0b0a33dd31a
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-04 00:25:16 -0300

    Update outputs.tf

--- ecr-module-new/outputs.tf
+++ ecr-module-new/outputs.tf
@@ -1,4 +1,9 @@
-# output "ecr_repository_url" {
-#   description = "A URL do repositório do ECR."
-#   value       = module.ecr_repository.ecr_repository_url
-# }
+output "repository_url" {
+  value       = aws_ecr_repository.private_repo.repository_url
+  description = "URL do repositório no ECR"
+}
+
+output "registry_id" {
+  value       = aws_ecr_repository.private_repo.registry_id
+  description = "ID do registro no ECR"
+}
