24e501717854f02fb60660ba9362446cf7101cd0
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-04 11:50:06 -0300

    Update outputs.tf

--- kms-module/outputs.tf
+++ kms-module/outputs.tf
@@ -1,3 +1,11 @@
-output "kms_key_arn" {
+output "arn" {
   value = aws_kms_key.this.arn
+}
+
+output "key_id" {
+  value = aws_kms_key.this.key_id
+}
+
+output "alias_name" {
+  value = aws_kms_alias.this.name
 }
