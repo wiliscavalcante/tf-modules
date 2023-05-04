8ccbf24b39cd3259e4a400e883fc684002c73148
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-03 23:23:30 -0300

    Update main.tf

--- ecr-module-new/main.tf
+++ ecr-module-new/main.tf
@@ -1,15 +1,14 @@
-resource "aws_ecr_repository" "my_repo" {
-  name = var.repository_name
+resource "aws_ecr_repository" "ecr_repo" {
+  name                 = var.repository_name
+  image_scanning_configuration {
+    scan_on_push       = var.scan_on_push
+  }
+  image_tag_mutability = var.image_tag_mutability
 
-  image_scanning_configuration = var.image_scanning_configuration
-  image_tag_mutability         = var.image_tag_mutability
-  lifecycle_policy             = var.lifecycle_policy ?? ""
-
-  tags = merge({
-    "Name" = var.repository_name
-  }, var.repository_tags)
-}
-
-output "repository_url" {
-  value = aws_ecr_repository.my_repo.repository_url
+  dynamic "lifecycle_policy" {
+    for_each = var.lifecycle_policy_enabled == true ? [1] : []
+    content {
+      policy_text = var.lifecycle_policy_text
+    }
+  }
 }
