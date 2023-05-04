121d6d0c46ddbe3ad81e07115cc4969885d14976
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-04 09:59:29 -0300

    Update outputs.tf

--- ecr-module/outputs.tf
+++ ecr-module/outputs.tf
@@ -1,18 +1,9 @@
-################################################################################
-# Repository (Public and Private)
-################################################################################
+output "repository_url" {
+  value       = aws_ecr_repository.this.repository_url
+  description = "URL do repositório no ECR"
+}
 
-# output "repository_arn" {
-#   description = "Full ARN of the repository"
-#   value       = try(aws_ecr_repository.this[0].arn, aws_ecrpublic_repository.this[0].arn, null)
-# }
-
-# output "repository_registry_id" {
-#   description = "The registry ID where the repository was created"
-#   value       = try(aws_ecr_repository.this[0].registry_id, aws_ecrpublic_repository.this[0].registry_id, null)
-# }
-
-# output "repository_url" {
-#   description = "The URL of the repository"
-#   value       = try(aws_ecr_repository.this[0].repository_url, aws_ecrpublic_repository.this[0].repository_uri, null)
-# }
+output "registry_id" {
+  value       = aws_ecr_repository.this.registry_id
+  description = "ID do registro no ECR"
+}
