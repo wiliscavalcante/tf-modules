c3079ddacffae7d48ddaabd35e9700b65ce4dd7e
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-03 23:37:30 -0300

    Update variables.tf

--- ecr-module-new/variables.tf
+++ ecr-module-new/variables.tf
@@ -1,28 +1,28 @@
 variable "repository_name" {
+  description = "O nome do repositório do ECR."
   type        = string
-  description = "O nome do repositório ECR"
 }
 
 variable "scan_on_push" {
+  description = "Habilita ou desabilita a verificação de imagem na submissão."
   type        = bool
-  default     = true
-  description = "Indica se as imagens devem ser escaneadas após serem enviadas para o repositório"
+  default     = false
 }
 
 variable "image_tag_mutability" {
+  description = "O tipo de mutabilidade das tags de imagem."
   type        = string
   default     = "MUTABLE"
-  description = "Indica se as tags de imagem podem ser alteradas"
-}
-
-variable "lifecycle_policy_enabled" {
-  type        = bool
-  default     = false
-  description = "Indica se uma política de ciclo de vida está habilitada para o repositório"
 }
 
 variable "lifecycle_policy_text" {
+  description = "O texto da política de ciclo de vida do ECR."
   type        = string
-  default     = ""
-  description = "O texto da política de ciclo de vida para o repositório"
+  default     = null
+}
+
+variable "lifecycle_policy_enabled" {
+  description = "Habilita ou desabilita a política de ciclo de vida."
+  type        = bool
+  default     = false
 }
