9604dc3abd3cb284a0e1d928e4281b38fa7a3a89
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-04 23:25:28 -0300

    Update outputs.tf

--- efs-module/outputs.tf
+++ efs-module/outputs.tf
@@ -1,11 +1,11 @@
 output "efs_id" {
-  value = aws_efs_file_system.efs_volume.id
+  value = aws_efs_file_system.this.id
 }
 
 output "efs_dns_name" {
-  value = aws_efs_file_system.efs_volume.dns_name
+  value = aws_efs_file_system.this.dns_name
 }
 
 output "efs_mount_target_ids" {
-  value = aws_efs_file_system.efs_volume.mount_target_ids
+  value = aws_efs_file_system.this.mount_target_ids
 }
