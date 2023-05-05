637e597670adad5cb6b635a5fce4cf39b9be8c0b
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-04 21:37:56 -0300

    Delete ecr-module-new directory

--- ecr-module-new/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "repository_url" {
-  value       = aws_ecr_repository.this.repository_url
-  description = "URL do repositório no ECR"
-}
-
-output "registry_id" {
-  value       = aws_ecr_repository.this.registry_id
-  description = "ID do registro no ECR"
-}
