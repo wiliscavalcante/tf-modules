924940666128dda53abd2d69828cadfdf747c541
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-04 21:57:03 -0300

    Delete s3-bucket-module/exemplo directory

--- s3-bucket-module/exemplo/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "tags" {
-  description = "(Optional) A mapping of tags to assign to the bucket."  
-  type        = map(any)
-  default = {
-    environment = "prod"
-    terraform   = "true"
-  }
-}
