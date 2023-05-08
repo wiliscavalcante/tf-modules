a3e1c9e10c1e5f10fb154dbeeeaa726246953d59
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-05-08 11:28:15 -0300

    Update variables.tf

--- kms-module/variables.tf
+++ kms-module/variables.tf
@@ -46,8 +46,8 @@ variable "multi_region" {
 }
 
 variable "policy" {
-  type        = any
-  description = "A política de acesso à chave KMS."
+  type        = string
+  description = "Um documento JSON de política válido. Esta é uma política de chave, não uma política IAM."
 }
 
 variable "tags" {
