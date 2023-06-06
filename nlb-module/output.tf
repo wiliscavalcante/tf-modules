fbb8f0943313f59a0a4e45f4dacc453db6f1dfb1
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-06-06 18:27:37 -0300

    Update output.tf

--- nlb-module/output.tf
+++ nlb-module/output.tf
@@ -2,3 +2,8 @@ output "nlb_arn" {
   description = "ARN do Network Load Balancer"
   value       = aws_lb.network.arn
 }
+
+output "dns_name" {
+  description = "Nome DNS do NLB"
+  value       = aws_lb.internal_nlb.dns_name
+}
