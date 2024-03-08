9b62759e64dd02ce5458922b8542461b874f5c0c
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2024-03-07 23:20:22 -0300

    Update main.tf

--- permissions-aws/main.tf
+++ permissions-aws/main.tf
@@ -1,3 +1,30 @@
+###cloudwatch-logs.tf
+resource "aws_iam_role_policy" "application_policy_cloudwatch_logs" {
+  count = length(var.cloudwatch_logs) > 0 ? 1 : 0
+  name  = "BUPolicyFor${replace(title(replace(var.app_name, "-", " ")), " ", "")}CloudWatchLogs"
+  role  = aws_iam_role.application_role.id
+
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      for logs in var.cloudwatch_logs :
+      {
+        Effect = "Allow",
+        Action = flatten([
+          logs.permission.put ? [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ] : []
+        ])
+        Resource = [
+          "arn:aws:logs:${logs.id.region}:${logs.id.account_id}:log-group:${logs.id.logGroup}",
+          "arn:aws:logs:${logs.id.region}:${logs.id.account_id}:log-group:${logs.id.logGroup}:log-stream:${logs.id.logStream}"
+        ]
+      }
+    ]
+  })
+}
 ###dynamodb.tf
 
 resource "aws_iam_role_policy" "application_policy_dynamodb" {
@@ -140,35 +167,49 @@ output "application_role" {
 ###role.tf
 
 resource "aws_iam_role" "application_role" {
-    name = "BURoleFor${replace(title(replace(var.app_name, "-", " ")), " ", "")}${upper(var.env)}"
-  
-    assume_role_policy = jsonencode({
-      Version   = "2012-10-17"
-      Statement = [
-        {
-          Effect    = "Allow"
-          Principal = {
-            Federated = "arn:aws:iam::${var.account_id}:oidc-provider/${var.oidc_provider}"
-          }
-          Action    = "sts:AssumeRoleWithWebIdentity"
-          Condition = {
-            StringEquals = {
-              "${var.oidc_provider}:aud" = "sts.amazonaws.com",
-              "${var.oidc_provider}:sub" = "system:serviceaccount:${var.namespace}:${var.app_name}"
-            }
-          }
-        },
-      ]
-    })
-  
-    permissions_boundary = "arn:aws:iam::${var.account_id}:policy/BUAdminBasePolicy"
-  
-    tags = merge(
-      var.extra_tags,
+  name = "BURoleFor${replace(title(replace(var.app_name, "-", " ")), " ", "")}${upper(var.env)}"
+
+
+  assume_role_policy = local.is_eks ? jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
       {
-        Terraformed = "true"
-      })
-  }
+        Effect    = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${var.account_id}:oidc-provider/${local.eks.oidc_provider}"
+        }
+        Action    = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${local.eks.oidc_provider}:aud" = "sts.amazonaws.com",
+            "${local.eks.oidc_provider}:sub" = "system:serviceaccount:${local.eks.namespace}:${var.app_name}"
+          }
+        }
+      },
+    ]
+  }) : jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      for aws_service in var.aws.services :
+      {
+        Effect    = "Allow",
+        Principal = {
+          Service = "${aws_service}.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  permissions_boundary = "arn:aws:iam::${var.account_id}:policy/BUAdminBasePolicy"
+
+  tags = merge(
+    var.extra_tags,
+    {
+      Terraformed = "true"
+    }
+  )
+}
 
   ###s3.tf
 
@@ -285,190 +326,181 @@ resource "aws_iam_role_policy" "application_policy_sqs" {
 ###variables.tf
 
 variable "account_id" {
-    type = string
-  }
-  
-  variable "env" {
-    type = string
-  }
-  
-  variable "namespace" {
-    type = string
-  }
-  
-  variable "app_name" {
-    type = string
-  }
-  
-  variable "oidc_provider" {
-    type = string
-  }
-  
-  variable "extra_tags" {
-    type    = map(string)
-    default = {}
-  }
-  
-  variable "sqs_queues" {
-    type = list(
-      object({
-        name       = string
-        region     = string
-        permission = object({
-          send    = bool
-          receive = bool
-        })
-      }))
-    default = []
-  }
-  
-  variable "sns_regions_send_sms" {
-    type    = list(string)
-    default = []
-  }
-  
-  variable "sns_topics" {
-    type = list(
-      object({
-        name       = string
-        region     = string
-        permission = object({
-          publish = bool
-        })
-      }))
-    default = []
-  }
-  
-  variable "s3_buckets" {
-    type = list(
-      object({
-        name       = string
-        permission = object({
-          read   = bool
-          write  = bool
-          delete = bool
-        })
-      }))
-    default = []
-  }
-  
-  variable "aws_keyspaces" {
-    type = list(
-      object({
-        name       = string
-        region     = string
-        tables     = list(string)
-        permission = object({
-          select               = bool
-          insert_update_delete = bool
-          create_alter_table   = bool
-          admin                = bool
-        })
-      }))
-    default = []
-  }
-  
-  variable "dynamodb_tables" {
-    type = list(
-      object({
-        name       = string
-        region     = string
-        permission = object({
-          select = bool
-          insert = bool
-          update = bool
-          delete = bool
-          admin  = bool
-        })
-      }))
-    default = []
-  }
-  
-  variable "kms_keys" {
-    type = list(
-      object({
-        id         = map(string)
-        permission = object({
-          encrypt = bool
-          decrypt = bool
-        })
-      }))
-    default = []
-  }
-  
-  variable "secrets" {
-    type = list(
-      object({
-        id         = map(string)
-        permission = object({
-          read = bool
-        })
-      }))
-    default = []
-  }
-  
-  variable "lambda_functions" {
-    type = list(
-      object({
-        id = object({
-          name       = string
-          region     = string
-          account_id = string
-        })
-        permission = object({
-          invoke = bool
-        })
-      }))
-    default = []
-  }
-########################
-
-variable "service" {
-  description = "O serviço AWS que pode assumir esta role, se diferente de EKS."
-  type        = string
-  default     = ""
+  type = string
 }
 
-resource "aws_iam_role" "application_role" {
-  name = "BURoleFor${replace(title(replace(var.app_name, "-", " ")), " ", "")}${upper(var.env)}"
+variable "env" {
+  type = string
+}
 
-  assume_role_policy = var.service != "" ? jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "${var.service}.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  }) : jsonencode({
-    Version   = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = {
-          AWS       = "arn:aws:iam::${var.account_id}:root",
-          Federated = "arn:aws:iam::${var.account_id}:oidc-provider/${var.oidc_provider}"
-        },
-        Action    = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          StringEquals = {
-            "${var.oidc_provider}:aud" = "sts.amazonaws.com",
-            "${var.oidc_provider}:sub" = "system:serviceaccount:${var.namespace}:${var.app_name}"
-          }
-        }
-      }
-    ]
+variable "namespace" {
+  type        = string
+  default     = ""
+  # deprecation_message = "Use the object EKS.namespace"
+}
+
+variable "app_name" {
+  type = string
+}
+
+variable "oidc_provider" {
+  type        = string
+  default     = ""
+  # deprecation_message = "Use the object EKS.oidc_provider"
+}
+
+variable "eks" {
+  type = object({
+    namespace     = string
+    oidc_provider = string
   })
 
-  permissions_boundary = "arn:aws:iam::${var.account_id}:policy/BUAdminBasePolicy"
+  default = {
+    namespace     = ""
+    oidc_provider = ""
+  }
+}
 
-  tags = merge(
-    var.extra_tags,
-    {
-      Terraformed = "true"
-    }
-  )
+variable "aws" {
+  type = object({
+    services = list(string)
+  })
+
+  default = {
+    services = []
+  }
+}
+
+variable "extra_tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "sqs_queues" {
+  type = list(
+    object({
+      name       = string
+      region     = string
+      permission = object({
+        send    = bool
+        receive = bool
+      })
+    }))
+  default = []
+}
+
+variable "sns_regions_send_sms" {
+  type    = list(string)
+  default = []
+}
+
+variable "sns_topics" {
+  type = list(
+    object({
+      name       = string
+      region     = string
+      permission = object({
+        publish = bool
+      })
+    }))
+  default = []
+}
+
+variable "s3_buckets" {
+  type = list(
+    object({
+      name       = string
+      permission = object({
+        read   = bool
+        write  = bool
+        delete = bool
+      })
+    }))
+  default = []
+}
+
+variable "aws_keyspaces" {
+  type = list(
+    object({
+      name       = string
+      region     = string
+      tables     = list(string)
+      permission = object({
+        select               = bool
+        insert_update_delete = bool
+        create_alter_table   = bool
+        admin                = bool
+      })
+    }))
+  default = []
+}
+
+variable "dynamodb_tables" {
+  type = list(
+    object({
+      name       = string
+      region     = string
+      permission = object({
+        select = bool
+        insert = bool
+        update = bool
+        delete = bool
+        admin  = bool
+      })
+    }))
+  default = []
+}
+
+variable "kms_keys" {
+  type = list(
+    object({
+      id         = map(string)
+      permission = object({
+        encrypt = bool
+        decrypt = bool
+      })
+    }))
+  default = []
+}
+
+variable "secrets" {
+  type = list(
+    object({
+      id         = map(string)
+      permission = object({
+        read = bool
+      })
+    }))
+  default = []
+}
+
+variable "lambda_functions" {
+  type = list(
+    object({
+      id = object({
+        name       = string
+        region     = string
+        account_id = string
+      })
+      permission = object({
+        invoke = bool
+      })
+    }))
+  default = []
+}
+
+variable "cloudwatch_logs" {
+  type = list(
+    object({
+      id = object({
+        logGroup   = string
+        logStream  = string
+        region     = string
+        account_id = string
+      })
+      permission = object({
+        put = bool
+      })
+    }))
+  default = []
 }
