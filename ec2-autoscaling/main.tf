b5bb1d23ad596b68245853cfb507d34db952e93b
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2024-04-19 09:06:50 -0300

    Create main.tf

---
+++ ec2-autoscaling/main.tf
@@ -0,0 +1,54 @@
+resource "aws_launch_template" "app" {
+  name_prefix   = "lt-${var.project_name}-"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = var.volume_size
+      volume_type = "gp3"
+      encrypted   = true
+    }
+  }
+
+  vpc_security_group_ids = [var.security_group_id]
+
+  iam_instance_profile {
+    arn = var.iam_instance_profile_arn
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "Instance-${var.project_name}"
+    }
+  }
+
+  user_data = var.user_data
+
+  # Automaticamente atualiza a versão default para a versão mais recente do template
+  update_default_version = true
+}
+
+
+resource "aws_autoscaling_group" "app" {
+  launch_template {
+    id      = aws_launch_template.app.id
+    version = "$Latest"
+  }
+
+  min_size         = var.min_size
+  max_size         = var.max_size
+  desired_capacity = var.desired_capacity
+
+  vpc_zone_identifier = var.subnets
+  health_check_type   = "EC2"
+
+  tag {
+    key                 = "Name"
+    value               = "ASG-${var.project_name}"
+    propagate_at_launch = true
+  }
+}
